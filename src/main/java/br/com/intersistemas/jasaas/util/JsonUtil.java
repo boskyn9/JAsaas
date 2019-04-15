7f129b976435621ad6d54cbd98aeda6a0fcdfcbc
willian <willian@inter-02>
2019-04-15 16:29:26 -0300

    Adicionado teste para verificar data contida no JSON de retorno, para fazer o parse com date us ou date pt-br

--- src/main/java/br/com/intersistemas/jasaas/util/JsonUtil.java
+++ src/main/java/br/com/intersistemas/jasaas/util/JsonUtil.java
@@ -3,6 +3,8 @@ package br.com.intersistemas.jasaas.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.lang.reflect.Type;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author bosco
@@ -10,19 +12,26 @@ import java.lang.reflect.Type;
 public class JsonUtil {
 
     private static Gson gson = null;
-    
+
     public static Object parse(String json, Class clazz) {
         return parse(json, clazz, false);
     }
-    
+
     public static Object parse(String json, Class clazz, Boolean showLog) {
-        if(gson == null)
+        String dateFormat = "";
+        if (isDateBR(json)) {
+            dateFormat = "dd/MM/yyyy";
+        } else {
+            dateFormat = "yyyy-MM-dd";
+        }
+        if (gson == null) {
             gson = new GsonBuilder()
                     .excludeFieldsWithoutExposeAnnotation()
-                    .setDateFormat("yyyy-MM-dd").create();
+                    .setDateFormat(dateFormat).create();
+        }
         try {
             return gson.fromJson(json, clazz);
-        } catch(Exception e) {
+        } catch (Exception e) {
             if (showLog) {
                 System.out.println("Error parse gson.fromJson\n" + json);
                 e.printStackTrace();
@@ -30,21 +39,30 @@ public class JsonUtil {
             return null;
         }
     }
-    
+
     public static Object parse(String json, Type type) {
-        if(gson == null)
+        if (gson == null) {
             gson = new GsonBuilder()
                     .excludeFieldsWithoutExposeAnnotation()
                     .setDateFormat("yyyy-MM-dd").create();
+        }
         return gson.fromJson(json, type);
     }
 
     public static String toJSON(Object obj) {
-        if(gson == null)
+        if (gson == null) {
             gson = new GsonBuilder()
                     .excludeFieldsWithoutExposeAnnotation()
                     .setDateFormat("yyyy-MM-dd").create();
+        }
         return gson.toJson(obj);
     }
 
+    private static Boolean isDateBR(String json) {
+        Pattern pattern = Pattern.compile("^[0-9]{2}/[0-9]{2}/[0-9]{4}$");
+        Matcher matcher = pattern.matcher(json);
+        return matcher.matches();
+//        String dateUS = "2018-01-01";
+//        pattern = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+    }
 }
