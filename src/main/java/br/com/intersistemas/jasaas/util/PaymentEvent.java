5a2dcc3d1502fb7bcb46f11bca825b77ee60ae3e
Felipe <fefb08@gmail.com>
2022-04-30 12:04:28 -0300

    Adicionando novos eventos no PaymentEvent

--- src/main/java/br/com/intersistemas/jasaas/util/PaymentEvent.java
+++ src/main/java/br/com/intersistemas/jasaas/util/PaymentEvent.java
@@ -13,8 +13,14 @@ public enum PaymentEvent {
     PAYMENT_RECEIVED,// - Cobrança recebida.
     PAYMENT_OVERDUE,// - Cobrança vencida.
     PAYMENT_DELETED,// - Cobrança removida.
+    PAYMENT_RESTORED,// - Cobrança restaurada.
     PAYMENT_REFUNDED,// - Cobrança estornada.
+    PAYMENT_RECEIVED_IN_CASH_UNDONE,// - Recebimento em dinheiro desfeito.
     PAYMENT_CHARGEBACK_REQUESTED,// - Recebido chargeback.
     PAYMENT_CHARGEBACK_DISPUTE,// - Em disputa de chargeback (caso sejam apresentados documentos para contestação).
-    PAYMENT_AWAITING_CHARGEBACK_REVERSAL// - Disputa vencida, aguardando repasse da adquirente.
+    PAYMENT_AWAITING_CHARGEBACK_REVERSAL,// - Disputa vencida, aguardando repasse da adquirente.
+    PAYMENT_DUNNING_RECEIVED, // - Recebimento de negativação.
+    PAYMENT_DUNNING_REQUESTED,// - Requisição de negativação.
+    PAYMENT_BANK_SLIP_VIEWED,// - Boleto da cobrança visualizado pelo cliente.
+    PAYMENT_CHECKOUT_VIEWED, // - Fatura da cobrança visualizada pelo cliente.
 }
